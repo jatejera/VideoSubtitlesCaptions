c45cb0ffc8669ed2abf2f07f4eaffe92f1a9663e
Julian A Tejera <jats.tejera@gmail.com>
2018-05-09 16:39:29 -0400

    jQuery Version
    The jQuery version was downgraded from 3.3.1 to 2.2.4 to make it compatible with old bootstrap sites.

--- js/VideoWidget.js
+++ js/VideoWidget.js
@@ -1,13 +1,16 @@
 var VideoHead = document.getElementsByTagName('head')[0];
 var jqueryCDN = document.createElement('script');
 jqueryCDN.crossOrigin = 'anonymous';
-jqueryCDN.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+jqueryCDN.src = "https://code.jquery.com/jquery-2.2.4.min.js"; //compatible with old versions of bootstrap
 jqueryCDN.id = "videojQuery";
-jqueryCDN.integrity = 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=';
+jqueryCDN.integrity = 'sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=';
 VideoHead.appendChild(jqueryCDN);
 
 var VideoStyle = document.createElement('link');
-VideoStyle.href = "css/VideoWidget.css";
+
+VideoStyle.href = "./css/VideoWidget.css"; //absolute link to this CSS
+
 VideoStyle.rel = "stylesheet";
 VideoStyle.id = "videoStyle";
+VideoStyle.crossOrigin = "anonymous";
 VideoHead.appendChild(VideoStyle);
